540492b31d6bb2ff8e37251607d450ef007f4cf8
Adamczak <adamus.pl@gmail.com>
2023-06-10 00:35:08 +0200

    polish language added in API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c&lang=pl`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=pl&appid=895284fb2d2c50a520ea537456963d9c`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -52,7 +52,7 @@ function App() {
             </div>
             <div className="wind">
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} km/h</p> : null}
-              <p>Prędkośc wiatru</p>
+              <p>Prędkość wiatru</p>
             </div>
           </div>
         }
